1222ffee80efcc120b94fd7e78028b72509bb6ac
daveaugerinos <dave.augerinos@gmail.com>
2017-03-14 12:55:52 -0700

    Intial setup

---
+++ Stretch-My-Header/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stretch-My-Header
+//
+//  Created by Dave Augerinos on 2017-03-14.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
